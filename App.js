b945cf899ab18c2eda41b8e000496cb8496d7223
RodrigoLFarias <rodrigoenriquefarias@gmail.com>
2024-06-03 22:12:13 -0300

    adicionando melhorias

--- App.js
+++ App.js
@@ -1,73 +1,98 @@
-import { TextInput, TouchableOpacity } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from 'react'; // Importando o React
+import { ImageBackground, TextInput, TouchableOpacity, StyleSheet, Text, View } from 'react-native'; // Importando os componentes necessários do React Native
+import Icon from 'react-native-vector-icons/Ionicons'; // Importando o ícone Ionicons
+import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Importando o ícone FontAwesome
 
-export default function App() {
+export default function App(){ // Definindo a função do componente App
+  const imagemFundo = require("./src/img/florzinhaeborboleta.jpg"); // Importando a imagem de fundo
+
   return (
-    <View style={styles.container}>
-      <Text> Adicionando Ícones </Text>
+    <View style={styles.container}> {/* View principal */}
+      <ImageBackground source={imagemFundo} style={styles.ImagemB}> {/* Imagem de fundo */}
+        <Text style={styles.Texticon}> Bem-Vindo </Text> {/* Texto "Adicionando Ícones" */}
 
-        <View style = {styles.inputs}>
-            <View style = {styles.boxInput}>
-              <Icon name="person-circle-outline" size={35}> </Icon>
-              <TextInput style= {styles.input} placeholder='Digite seu nome'/>
-            </View>
+        <View style={styles.inputs}> {/* Contêiner dos campos de entrada */}
+          <View style={styles.boxInput}> {/* Contêiner de cada campo de entrada */}
+            <Icon name="person-circle-outline" size={24.5} color='white'/> {/* Ícone do usuário */}
+            <TextInput style={styles.input} placeholder='Digite seu nome' placeholderTextColor='white' /> {/* Campo de entrada para o nome */}
+          </View>
 
-            <View style = {styles.boxInput}>
-              <FontAwesome name="lock" size={35} />
-              <TextInput style= {styles.input} placeholder='Digite sua senha'/>
-            </View>
+          <View style={styles.boxInput}> {/* Contêiner de cada campo de entrada */}
+            <FontAwesome name="lock" size={24.5} color='white' /> {/* Ícone do cadeado */}
+            <TextInput style={styles.input} placeholder='Digite sua senha' placeholderTextColor='white' /> {/* Campo de entrada para a senha */}
+          </View>
         </View>
-        <TouchableOpacity style = {styles.btn}>
-           <Text style = {styles.txtBtn}> Acessar</Text>
+        <TouchableOpacity style={styles.btn}> {/* Botão de acesso */}
+          <Text style={styles.txtBtn}> Acessar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style = {styles.btn}>
-          <FontAwesome name="google" size={35} color= 'white' />
-          <Text style = {styles.txtBtn}> Acessar com o Google</Text>
+        <TouchableOpacity style={styles.btn}> {/* Botão de acesso com o Google */}
+          <FontAwesome name="google" size={30} color='white' />
+          <Text style={styles.txtBtn}> Acessar com o Google</Text>
         </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}> {/* Botão de acesso com o Facebook */}
+        <FontAwesome name="facebook" size={30} color='white'/>
+          <Text style={styles.txtBtn}> Acessar com o Facebook</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}> {/* Botão "Esqueceu a senha?" */}
+          {/* <FontAwesome name="google" size={30} color='white' /> */}
+          <Text style={styles.txtBtn}>Esqueceu a senha?</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ // Estilos CSS
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-    gap: 10,
-
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fundo semi-transparente
+  },
+  Texticon: {
+    color: '#FFf', // Cor do texto
+    fontFamily: 'Sans-Serif', // Fonte do texto
+    fontSize: 25 // Tamanho do texto
   },
   inputs: {
     gap: 6,
-    width: 300
+    width: 300,
   },
   boxInput: {
-  flexDirection: 'row',
-  justifyContent: 'center',
-  gap: 20,
-  },
-
-  input:{
- borderWidth: 1,
-  padding: 5,
-  width: '100%',
-
-  },
-
-  btn:{
     flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+  },
+  input: {
+    borderWidth: 0.5,
+    padding: 5,
+    flex: 1,
+    color: 'white',
+  },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 10,
-    padding: cls10,
+    padding: 5,
     borderRadius: 6,
-    backgroundColor: '#4287f5'
-
+    backgroundColor: '#4287f5',
+    borderWidth: 0.5,
+    borderColor: 'black'
   },
-  txtBtn:{
-  color: 'black'
+  txtBtn: {
+    color: 'white',
   },
-  
-
+  ImagemB: {
+    resizeMode: 'cover',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    gap: 10,
+    height: '100%',
+    width: "100%"
+  }
 });
+
+
